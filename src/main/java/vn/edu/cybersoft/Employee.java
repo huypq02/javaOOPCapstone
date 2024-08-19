e56eccc4c0ed22513a3dd2bf0620757e85ae236d
Huy Pham <huypham0297@gmail.com>
2024-08-20 00:05:44 +0700

    chore: add some validator for input

--- src/main/java/vn/edu/cybersoft/Employee.java
+++ src/main/java/vn/edu/cybersoft/Employee.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import vn.edu.cybersoft.humanResourceValidator;
+import vn.edu.cybersoft.validator.humanResourceValidator;
 import vn.edu.cybersoft.validator.humanResourceValidator;
 
 public abstract class Employee {
